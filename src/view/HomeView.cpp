173b1303578a18e8031993c2be482893354c2abe
Paweł <pawelbudzik975@gmail.com>
2026-04-11 13:07:51 +0200

    costam

--- src/view/HomeView.cpp
+++ src/view/HomeView.cpp
@@ -7,14 +7,24 @@ HomeView::HomeView(wxWindow *window, Router *router) : wxPanel(window) {
   wxBoxSizer *mainSizer = new wxBoxSizer(wxVERTICAL);
   wxBoxSizer *titleRow = new wxBoxSizer(wxHORIZONTAL);
 
-  wxStaticText *labelTitle = new wxStaticText(this, wxID_ANY, "Home page");
+  wxStaticText *labelTitle = new wxStaticText(this, wxID_ANY, "Venicle Management App");
 
-  changePage = new wxButton(this, wxID_ANY, "Zmien");
+  changePage = new wxButton(this, wxID_ANY, "Logout", wxDefaultPosition, wxSize(80, -1));
 
   titleRow->Add(labelTitle, 0, wxALL | wxALIGN_CENTER_VERTICAL, 5);
-  titleRow->Add(changePage, 1, wxALL | wxEXPAND, 5);
+  titleRow->AddStretchSpacer();  // Rozciąga przestrzeń między tytułem a przyciskiem
+  titleRow->Add(changePage, 0, wxALL | wxALIGN_CENTER_VERTICAL, 5);
 
   mainSizer->Add(titleRow, 0, wxEXPAND | wxALL, 5);
+
+  // Nowy panel (bez przycisku)
+  wxPanel *newPanel = new wxPanel(this, wxID_ANY);
+  newPanel->SetBackgroundColour(*wxBLUE);
+  wxBoxSizer *newPanelSizer = new wxBoxSizer(wxVERTICAL);
+  wxStaticText *newText = new wxStaticText(newPanel, wxID_ANY, "Nowy panel w home!");
+  newPanelSizer->Add(newText, 0, wxALL | wxALIGN_CENTER, 10);
+  newPanel->SetSizer(newPanelSizer);
+  mainSizer->Add(newPanel, 1, wxEXPAND | wxALL, 5);
 
   this->SetSizer(mainSizer);
   mainSizer->Fit(this);
