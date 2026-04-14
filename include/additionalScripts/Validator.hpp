4c089cf2da1a87454229cf363317768d8b860f84
Ala <al.fryndt@gmail.com>
2026-04-14 17:45:35 +0200

    Create Validator class .hpp file

---
+++ include/additionalScripts/Validator.hpp
@@ -0,0 +1,27 @@
+#ifndef VALIDATOR_HPP
+#define VALIDATOR_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Logger.hpp"
+
+class Validator {
+    private:
+        Logger& logger;
+        std::vector<std::string> errors;
+
+    public:
+        Validator(Logger& loggerInstance);
+
+        bool validateEmail(std::string& email);
+
+        bool validatePassword(std::string& password);
+
+        bool checkEmpty (std::string& value);
+
+        std::vector<std::string> getErrors();
+};
+
+
+#endif
