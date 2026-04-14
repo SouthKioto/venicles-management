2c0351970c69dbf4209595198de36c1bf269ba12
nyxxor <dglowa2005@gmail.com>
2026-04-14 10:35:26 +0200

    attempt to repair

--- src/model/VehicleModel.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/model/VehicleModel.hpp"
-
-void VehicleModel::setVehicle(Vehicle *vehicle) { this->vehicle = vehicle; }
-
-Vehicle *VehicleModel::getVehicle() const { return vehicle; }
-
-void VehicleModel::clearVehicle() { this->vehicle = nullptr; }
