0bd2aa5139a1a4ec71da13d9b46a960c60dd0892
SouthKioto <bolisegabartek6@gmail.com>
2026-04-16 22:42:33 +0200

    naprawa konfliktów w mainview

--- src/view/MainView.cpp
+++ src/view/MainView.cpp
@@ -2,14 +2,14 @@
 #include "../include/classes/Router.hpp"
 #include "../include/controller/HomeController.hpp"
 #include "../include/controller/LoginController.hpp"
+#include "../include/controller/RegisterController.hpp"
 #include "../include/database/Database.hpp"
 #include "../include/model/HomeModel.hpp"
 #include "../include/model/LoginModel.hpp"
+#include "../include/model/RegisterModel.hpp"
 #include "../include/view/HomeView.hpp"
 #include "../include/view/LoginView.hpp"
 #include "../include/view/RegisterView.hpp"
-#include "../include/model/RegisterModel.hpp"
-#include "../include/controller/RegisterController.hpp"
 
 MainView::MainView(Logger *logger, Database *database)
     : wxFrame(NULL, wxID_ANY, "Venicle Management App") {
@@ -23,8 +23,7 @@ MainView::MainView(Logger *logger, Database *database)
   router = new Router(container, logger);
 
   // modele
-<<<<<<< HEAD
-  LoginModel *loginModel = new LoginModel();
+  LoginModel *loginModel = new LoginModel(database, logger);
   HomeModel *homeModel = new HomeModel();
   RegisterModel *registerModel = new RegisterModel();
 
@@ -44,23 +43,6 @@ MainView::MainView(Logger *logger, Database *database)
   router->add("login", loginView);
   router->add("home", homeView);
   router->add("register", registerView);
-=======
-  LoginModel loginModel(database, logger);
-  HomeModel homeModel;
-
-  // widoki
-  LoginView loginView(container, router);
-  HomeView homeView(container, router);
-
-  // kotrolery
-  new LoginController(&loginModel, &loginView, router);
-  new HomeController(&homeView, &homeModel, router);
-
-  homeView.Hide();
-
-  router->add("login", &loginView);
-  router->add("home", &homeView);
->>>>>>> master
 
   router->navigate("login");
 
