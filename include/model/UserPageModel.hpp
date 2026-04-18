fa2c22da7af79cdbf71dff479eeb842dfb4c16f3
Bartomiej Bolisega <bolisegabartek6@gmail.com>
2026-04-18 16:15:29 +0200

    Zmiany w userpagemodel

--- include/model/UserPageModel.hpp
+++ include/model/UserPageModel.hpp
@@ -1,6 +1,7 @@
 #ifndef USER_PAGE_MODEL_HPP
 #define USER_PAGE_MODEL_HPP
 
+#include "../include/additionalScripts/Logger.hpp"
 #include "../include/classes/Router.hpp"
 #include "../include/classes/User.hpp"
 #include "../include/classes/Vehicle.hpp"
@@ -11,18 +12,19 @@ private:
   User *user;
   Router *router;
   Database *database;
+  Logger *logger;
 
   bool isAdmin = false;
   std::vector<Vehicle *> userVehicles;
 
 public:
-  UserPageModel(User *user, Router *router, Database *database);
+  UserPageModel(User *user, Router *router, Database *database, Logger *logger);
   ~UserPageModel();
 
-  void showUserData();
-  void checkAdmin(User *user);
-  void showAdminSettings(User *user);
-  void getMyVehicles();
+  void getUserData();
+  void checkAdmin();
+  void showAdminSettings();
+  void getUserVehicles();
 };
 
 #endif // !USER_PAGE_MODEL_HPP
--- src/model/UserPageModel.cpp
+++ src/model/UserPageModel.cpp
@@ -1,4 +1,36 @@
 #include "../include/model/UserPageModel.hpp"
 
-UserPageModel::UserPageModel(User *user, Router *router, Database *database)
-    : user(user), router(router), database(database) {}
+UserPageModel::UserPageModel(User *user, Router *router, Database *database,
+                             Logger *logger)
+    : user(user), router(router), database(database), logger(logger) {}
+
+void UserPageModel::checkAdmin() {
+
+  // INFO: inaczej sprawdzac isAdmin u usera
+
+  this->isAdmin = false;
+
+  auto mapToUser = [](sqlite3_stmt *stmt) -> User {
+    User user;
+    user.setAdminPermission((bool)sqlite3_column_text(stmt, 0));
+
+    return user;
+  };
+
+  std::string sql =
+      "SELECT isAdmin FROM users WHERE email=" + this->user->getEmail() + ";";
+
+  this->database->fetch<User>(sql, mapToUser);
+}
+
+void UserPageModel::showAdminSettings() {
+  if (this->isAdmin) {
+    std::cout << "Ustawienia administratorskie wlaczone flaga" << std::endl;
+  } else {
+    this->logger->log(LogLevel::Info, "No access");
+  }
+}
+
+void UserPageModel::getUserData() {}
+
+void UserPageModel::getUserVehicles() {}
