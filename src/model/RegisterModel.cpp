f0f507d80e3b2623441fae70b87747da3f558fff
nyxxor <dglowa2005@gmail.com>
2026-04-25 21:14:53 +0200

    zmiana komentarzy

--- src/model/RegisterModel.cpp
+++ src/model/RegisterModel.cpp
@@ -17,7 +17,6 @@ bool RegisterModel::checkUserExists(const std::string& email) {
         return user;
     };
 
-    // INFO: Sprawdzamy czy email już istnieje w bazie
     std::string sql = "SELECT email FROM users WHERE email = '" + email + "' LIMIT 1;";
     std::vector<User> users = conn->fetch<User>(sql, mapToUser);
 
@@ -32,14 +31,11 @@ bool RegisterModel::checkUserExists(const std::string& email) {
 bool RegisterModel::registerUser(const std::string& name, const std::string& surname, const std::string& email, const std::string& password) {
     errors.clear();
 
-    // INFO: Wstawianie nowego użytkownika
-    // TODO: W przyszłości warto dodać hashownaie hasła przed zapisem do bazy
     std::string sql = "INSERT INTO users (name, surname, email, password) VALUES ('" + 
                       name + "', '" + surname + "', '" + email + "', '" + password + "');";
     
     conn->executeQuery(sql);
     
-    // Zakładamy sukces, jeśli executeQuery nie rzuciło błędu (choć Twoja klasa Database mogłaby zwracać bool)
     logger->log(LogLevel::Info, "Zarejestrowano nowego użytkownika: " + email);
     return true;
 }
